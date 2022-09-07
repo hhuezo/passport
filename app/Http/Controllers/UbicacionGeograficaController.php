6420a858a8dc56b8d9dccf81516c96a4612af713
hhuezo <hugo.alex.huezo@gmail.com>
2022-09-07 11:18:18 -0600

    get municipios

--- app/Http/Controllers/UbicacionGeograficaController.php
+++ app/Http/Controllers/UbicacionGeograficaController.php
@@ -11,9 +11,26 @@ class UbicacionGeograficaController extends Controller
 
     public function get_departamento(Request $request)
     {
-        $departamento = UbicacionGeografica::where('UGE_DGE_CODIGO','=','DP')->get();
+        $departamentos = UbicacionGeografica::where('UGE_DGE_CODIGO','=','DP')->get();
 
-        $response = array("val" => "0", "mensaje" =>  "OK","resultado"=>$departamento);
+        $response = array("val" => "0", "mensaje" =>  "OK","resultado"=>$departamentos);
+
+        return response()->json(
+            $response,
+            200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+
+    }
+
+    public function get_municipio($id)
+    {
+        $departamento = UbicacionGeografica::findOrFail($id);
+
+        $municipios = UbicacionGeografica::where('UGE_DGE_CODIGO','=','MU')->where('UGE_UGE_CODIGO','=',$departamento->UGE_CODIGO)->get();
+
+        $response = array("val" => "0", "mensaje" =>  "OK","resultado"=>$municipios);
 
         return response()->json(
             $response,
